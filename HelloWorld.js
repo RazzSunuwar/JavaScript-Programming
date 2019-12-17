b3338d6d1635fec85f8328799ae24ee175ca987d
Razz Sunuwar <55791502+RazzSunuwar@users.noreply.github.com>
2019-12-17 15:14:37 +0545

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,6 +1,6 @@
-var name; //memory allocation
-console.log('name is >>',name) // undefined
-var roll = 333;
-var status=  true;
+// var name; //memory allocation
+// console.log('name is >>',name) // undefined
+// var roll = 333;
+// var status=  true;
 
-const address = "abcd";
+// const address = "abcd";
