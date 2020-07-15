f3067629756abcf11ad96fe74d03f5e27b28cf83
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-15 22:31:46 +0545

    Modified codes

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,13 +1,13 @@
-// var name = "Ram"; //memory allocation
-// console.log('My name is >>',name);
-// var roll = 333;
-// var status=  true;
+var name = "Ram"; //memory allocation
+console.log('My name is >>',name);
+var roll = 333;
+var status=  true;
 
-// const address = "abcd";
+const address = "abcd";
 
-// var myWord = "Hello world";
+var myWord = "Hello world";
 
-// function myName(){
-//   console.log(myWord);
-// };
-// myName();        //Hello World
+function myName(){
+  console.log(myWord);
+};
+myName();        //Hello World
